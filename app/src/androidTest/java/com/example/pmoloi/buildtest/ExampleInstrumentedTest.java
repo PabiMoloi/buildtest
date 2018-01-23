2a19eac920d2edc4ac19bf5ef792dca3be6dbd0d
Paballo Moloi <PMoloi@jhb.dvt.co.za>
2018-01-23 10:28:01 +0200

    instrumented test build test

--- app/src/androidTest/java/com/example/pmoloi/buildtest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pmoloi/buildtest/ExampleInstrumentedTest.java
@@ -2,11 +2,19 @@ package com.example.pmoloi.buildtest;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +23,27 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.pmoloi.buildtest", appContext.getPackageName());
+    }
+    @Test
+    public void helloWorldDisplayTest()
+    {
+        Espresso.onView(withId(R.id.textViewHelloWorld)).check(matches(withText("Hello World!")));
+    }
+    @Test
+    public void buttonDoSomethingTest()
+    {
+        Espresso.onView(withId(R.id.buttonDoSomething)).perform(click());
     }
 }
